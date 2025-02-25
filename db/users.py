549e83a26a17ea577b2f1470bad8d438f757ab38
Rylynn Peters <chewynerd@Rylynns-MacBook-Air.local>
2025-02-25 15:04:09 -0700

    Moved assets into static to avoid future issues. Added two new fields to the user database. viewability, which is a list of users whos secondary users can view the user, and view-permissions, which is a list of users who the secondary users of the user can view. Added a function to change the values in these two fields.

--- db/users.py
+++ db/users.py
@@ -2,13 +2,15 @@ import tinydb
 
 '''
 This is a user. A user is their username, password, profile picture, background-color(background-color of their page),
-foreground color(color of the boxes containing text, videos, etc.), text-color, flag-image-url, friends,
+foreground color(color of the boxes containing text, videos, etc.), text-color, flag-image-url, friends, viewability,
 and favorites.
 '''
 
 
 #Creates a new user with username, password, and default attributes. If username != any other username,
 #returns the new user. Otherwise, returns None.
+#Viewability-list is a list of users whos subusers can view this user.
+#View-permissions-list is a list of users who this user's secondary users can view
 def new_user(db, username, password):
     users = db.table('users')
     User = tinydb.Query()
@@ -17,11 +19,13 @@ def new_user(db, username, password):
     user_record = {
             'username': username,
             'password': password,
-            'profile_pic_url': "assets/default_user.jpg",
+            'profile_pic_url': "static/assets/default_user.jpg",
             'background-color': "#000000",
             'foreground-color': "#FFFFFF",
             'text-color': "#000000",
-            'flag-image-url': "assets/default-flag.jpg",
+            'flag-image-url': "static/assets/default-flag.jpg",
+            'viewability-list': [],
+            'view-permissions-list': [],
             'friends': [],
             'favorites': []
             }
@@ -84,7 +88,7 @@ def get_user_friends(db, user):
 
 #Edits the user's profile picture. Replaces the current profile_pic_url with a new profile_pic_url].
 #MAKE SURE TO UPLOAD THE PROFILE PICTURE TO ASSETS BEFORE CALLING THIS FUNCTION!
-def edit_user_profile_pic(db, user, profile_pic_url):
+def set_user_profile_picture(db, user, profile_pic_url):
     users = db.table('users')
     User = tinydb.Query()
     user['profile_pic_url'] = profile_pic_url
@@ -94,7 +98,7 @@ def edit_user_profile_pic(db, user, profile_pic_url):
 
 #Given a new color, and the type of color to edit, updates the color of that type to the new color.
 #For color type, put in the type of color. For example: 'background-color', 'foreground-color', 'text-color'
-def edit_user_colors(db, user, color, color_type):
+def set_user_colors(db, user, color, color_type):
     users = db.table('users')
     User = tinydb.Query()
     user[color_type] = color
@@ -104,7 +108,7 @@ def edit_user_colors(db, user, color, color_type):
 
 #Edits the user's flag. Replaces the current flag_url with a new flag_url.
 #MAKE SURE TO UPLOAD THE FLAG TO ASSETS BEFORE CALLING THIS FUNCTION!
-def edit_user_flag(db, user, flag_url):
+def set_user_flag(db, user, flag_url):
     users = db.table('users')
     User = tinydb.Query()
     user['flag_url'] = flag_url
@@ -121,3 +125,37 @@ def add_favorite(db, user, favorite):
     users.upsert(user, (User.username == user['username']) &
                 (User.password == user['password']))
     return 'Favorite {}, added successfully!'.format(favorite[0] + ': ' + favorite[1]), 'success'
+
+#Takes a user, mode, and sub_mode
+#User is the user in the database that this function is editing
+#mode is the overall mode of this change. Is this affecting the subusers of this user (view-permissions-list),
+#or the subusers of other users? (viewability-list)
+#sub_mode is the category of viewability that is being applied:
+#friends- applied to all friends of this user
+#open- applied to all users
+#closed- list is empty
+def set_subuser_view_permission(db, user, mode, sub_mode):
+    users = db.table('users')
+    User = tinydb.Query()
+
+    if mode == "this_user":
+        if sub_mode == "friends":
+            for friend_username in user['friends']:
+                user['viewability-list'].append(friend_username)
+        elif sub_mode == "open":
+            for item in users.all():
+                user['viewability-list'].append(item)
+        else:
+            user['viewability-list'] = []
+    else:
+        if sub_mode == "friends":
+            for friend_username in user['friends']:
+                user['view-permissions-list'].append(friend_username)
+        elif sub_mode == "open":
+            for item in users.all():
+                user['view-permissions-list'].append(item)
+        else:
+            user['view-permissions-list'] = []
+    users.upsert(user, (User.username == user['username']) &
+                (User.password == user['password']))
+    return return_obj
