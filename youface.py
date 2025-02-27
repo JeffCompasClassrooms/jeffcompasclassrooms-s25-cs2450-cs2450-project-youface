70b6c90ddfe0ef8c17845c7b56939ab8b7c8dd36
Connor Denney <connordenney1031@gmail.com>
2025-02-27 16:06:01 -0700

    andded gambling and plinko code

--- youface.py
+++ youface.py
@@ -7,7 +7,8 @@ import timeago
 import tinydb
 
 # handlers
-from handlers import friends, login, posts
+from handlers import friends, login, posts, gambling
+
 
 app = flask.Flask(__name__)
 
@@ -19,6 +20,7 @@ def convert_time(ts):
 app.register_blueprint(friends.blueprint)
 app.register_blueprint(login.blueprint)
 app.register_blueprint(posts.blueprint)
+app.register_blueprint(gambling.blueprint)
 
 app.secret_key = 'mygroup'
 app.config['SESSION_TYPE'] = 'filesystem'
