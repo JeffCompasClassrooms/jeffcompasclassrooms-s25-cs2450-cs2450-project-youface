40a926279f8a2030e0c6551dc746877534937799
Creed Maxwell <creedmaxwell1@gmail.com>
2025-03-04 16:00:06 -0700

    delete button functionality added

--- handlers/posts.py
+++ handlers/posts.py
@@ -21,3 +21,21 @@ def post():
     posts.add_post(db, user, post)
 
     return flask.redirect(flask.url_for('login.index'))
+
+@blueprint.route('/postdelete', methods=['POST'])
+def delete():
+    """Deletes a post."""
+    db = helpers.load_db()
+
+    username = flask.request.cookies.get('username')
+    password = flask.request.cookies.get('password')
+
+    user = users.get_user(db, username, password)
+    if not user:
+        flask.flash('You need to be logged in to do that.', 'danger')
+        return flask.redirect(flask.url_for('login.loginscreen'))
+    
+    post_id = flask.request.form.get('post_id')
+    posts.delete_post(db, post_id)
+
+    return flask.redirect(flask.url_for('login.index'))
