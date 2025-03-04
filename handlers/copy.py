32fd6debeb879b2ba7327932047ee03ce302bb38
coderguysomething <jordancoleman370@gmail.com>
2025-03-04 11:56:10 -0700

    Made it so that you could get from the Register to Login page

--- handlers/copy.py
+++ handlers/copy.py
@@ -2,7 +2,6 @@ title = 'Taggle'
 title2 = 'Create User'
 subtitle = "From the stars to a single note we have a group for you!"
 subtitle2 = "Please pick one or more Tags."
-<<<<<<< HEAD
+
 #main
-=======
->>>>>>> main
+
